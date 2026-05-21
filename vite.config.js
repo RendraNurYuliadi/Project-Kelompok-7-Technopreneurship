472cd39ee99e508d6aa22d9508be471d475712cf
RendraNurYuliadi <rendra.education@gmail.com>
2026-05-21 19:06:43 +0700

    Update Vite base path for Vercel and rebuild docs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Project-Kelompok-7-Technopreneurship/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'docs'
